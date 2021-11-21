d314a6eaf4a83db864cc25602f7049473606c214
jypar <cholponova.zhypara@gmail.com>
2021-11-21 19:43:33 +0600

    Initial commit

---
+++ src/test/java/com/github/telegrambot/TelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
