85746a1f6d6e2f0a16e81cc529e319328e45ecdb
jypar <cholponova.zhypara@gmail.com>
2021-12-02 01:43:56 +0600

    changed FactsCommand

--- src/main/java/com/github/telegrambot/client/TelegramClient.java
+++ src/main/java/com/github/telegrambot/client/TelegramClient.java
@@ -1,27 +1,16 @@
 package com.github.telegrambot.client;
 
-import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
-import java.util.Arrays;
 import java.util.Map;
-
 
 public class TelegramClient {
 
     static final String URL_USERS = "https://cat-fact.herokuapp.com/facts/random?=cat&amount=1";
     RestTemplate restTemplate = new RestTemplate();
-    HttpHeaders headers = new HttpHeaders();
+
     public String getText(){
-
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<Map> response = restTemplate.exchange(URL_USERS,
-                HttpMethod.GET, entity, Map.class);
-
-        return (String) response.getBody().get("text");
-
+        Map<String,String> result = restTemplate.getForObject(URL_USERS,Map.class);
+        return  result.get("text");
     }
 
 }
--- src/main/java/com/github/telegrambot/command/FactsCommand.java
+++ src/main/java/com/github/telegrambot/command/FactsCommand.java
@@ -2,13 +2,14 @@ package com.github.telegrambot.command;
 
 import com.github.telegrambot.client.TelegramClient;
 import com.github.telegrambot.service.SendBotMessageService;
+import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class FactsCommand implements Command{
+import java.util.Map;
 
+public class FactsCommand implements Command{
     private final SendBotMessageService sendBotMessageService;
     TelegramClient telegramClient = new TelegramClient();
-    public final  String START_MESSAGE = telegramClient.getText();
 
     public FactsCommand(SendBotMessageService sendBotMessageService) {
         this.sendBotMessageService = sendBotMessageService;
@@ -16,6 +17,6 @@ public class FactsCommand implements Command{
 
     @Override
     public void execute(Update update) {
-        sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), START_MESSAGE);
+        sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), telegramClient.getText());
     }
 }
